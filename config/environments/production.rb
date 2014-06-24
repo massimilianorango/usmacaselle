cfa1a3b6f02f6a762cbc5fc2c5a4121a455360be
max <massimiliano.rango@gmail.com>
2014-06-24 15:34:29 +0200

    mailer gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Per i mailer: al posto di localhost metto il mio custom domain
-  #config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "http://trpkgyfc.herokuapp.com/" }
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV["SUPPORT_EMAIL"],
+    password:             ENV["SUPPORT_PASSWORD"],
+    authentication:       'plain',
+  enable_starttls_auto: true  }
 end
