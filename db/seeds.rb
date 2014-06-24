1faa6a88c469a6db27364a1b22c49a5e830bbda9
max <massimiliano.rango@gmail.com>
2014-06-24 02:43:25 +0200

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Sector.create(name: "Segreteria", email: "massimiliano.rango@gmail.com", password: "segreteria", password_confirmation: "segreteria", is_root: true)
+#Sector.create(name: "Segreteria", email: "segreteria@usmacaselle.it", password: "segreteria", password_confirmation: "segreteria", is_root: true)
+Sector.create(name: "Calcio", email: "calcio@usmacaselle.it", password: "calcio", password_confirmation: "calcio", is_root: false)
+Sector.create(name: "Basket", email: "basket@usmacaselle.it", password: "basket", password_confirmation: "basket", is_root: false)
+Sector.create(name: "Volley", email: "volley@usmacaselle.it", password: "volley", password_confirmation: "volley", is_root: false)
+Sector.create(name: "Atletica", email: "atletica@usmacaselle.it", password: "atletica", password_confirmation: "atletica", is_root: false)
+Sector.create(name: "Motoria", email: "motoria@usmacaselle.it", password: "motoria", password_confirmation: "motoria", is_root: false)
