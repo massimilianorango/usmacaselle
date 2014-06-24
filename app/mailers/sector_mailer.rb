3c152108691a28891b175738b856aef428f3e842
max <massimiliano.rango@gmail.com>
2014-06-24 16:18:27 +0200

    Mailer ok

--- app/mailers/sector_mailer.rb
+++ app/mailers/sector_mailer.rb
@@ -1,5 +1,5 @@
 class SectorMailer < ActionMailer::Base
-  default from: "support@usmacaselle.com"
+  default from: "assistenza.usmacaselle@gmail.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
