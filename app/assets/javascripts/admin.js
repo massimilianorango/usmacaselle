2a2b77c145cad2a648c568641113500918ea85c8
max <massimiliano.rango@gmail.com>
2014-07-01 11:33:44 +0200

    prova heroku3

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    var controller = $(".controller").text();
+    $(".sortable").sortable({
+        dropOnEmpty: false,
+        cursor: 'crosshair',
+        opacity: 0.4,
+        scroll: true,
+        revert: '200',
+        placeholder: controller + '-placeholder',
+        update: function(){
+            $.ajax({
+                type: 'post',
+                data: $(".sortable").sortable('serialize'),
+                dataType: 'script',
+                complete: function(request){
+                    $(".sortable").effect('highlight');
+                },
+            url: '/admin/items/sort'})
+        }
+    });
+});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,24 +42,3 @@ $(document).ready(function(){
     );
 })*/
 
-$(document).ready(function(){
-    var controller = $(".controller").text();
-    $(".sortable").sortable({
-        dropOnEmpty: false,
-        cursor: 'crosshair',
-        opacity: 0.4,
-        scroll: true,
-        revert: '200',
-        placeholder: controller + '-placeholder',
-        update: function(){
-            $.ajax({
-                type: 'post',
-                data: $(".sortable").sortable('serialize'),
-                dataType: 'script',
-                complete: function(request){
-                    $(".sortable").effect('highlight');
-                },
-            url: '/admin/items/sort'})
-        }
-    });
-});
