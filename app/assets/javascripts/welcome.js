2feef138ef76cba014fa0fd8248301aaa6271333
max <massimiliano.rango@gmail.com>
2014-07-28 19:05:57 +0200

    bugfix jquery?????

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,6 +1,38 @@
 $(document).ready(function() {
-    $('.post-content-public').ellipsis({
-        row: 5,
-        char: '**'
+    $(".post-text-public").dotdotdot({
+        /*  The HTML to add as ellipsis. */
+        ellipsis    : '... ',
+ 
+        /*  How to cut off the text/html: 'word'/'letter'/'children' */
+        wrap        : 'word',
+ 
+        /*  Wrap-option fallback to 'letter' for long words */
+        fallbackToLetter: true,
+ 
+        /*  jQuery-selector for the element to keep and put after the ellipsis. */
+        after       : "a.readmore",
+ 
+        /*  Whether to update the ellipsis: true/'window' */
+        watch       : "window",
+    
+        /*  Optionally set a max-height, if null, the height will be measured. */
+        height      : 180,
+ 
+        /*  Deviation for the height-option. */
+        tolerance   : 0,
+ 
+        /*  Callback function that is fired after the ellipsis is added,
+            receives two parameters: isTruncated(boolean), orgContent(string). */
+        callback    : function( isTruncated, orgContent ) {},
+ 
+        lastCharacter   : {
+ 
+            /*  Remove these characters from the end of the truncated text. */
+            remove      : [ ' ', ',', ';', '.', '!', '?' ],
+ 
+            /*  Don't add an ellipsis if this array contains 
+                the last character of the truncated text. */
+            noEllipsis  : []
+        }
     });
 });
