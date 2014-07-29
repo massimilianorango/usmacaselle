bfd94e659c46496ba38ea2df02131eb5aef0cf16
max <massimiliano.rango@gmail.com>
2014-07-29 11:50:46 +0200

    Solved Jquery plugins problem on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   #config.assets.compile = false
-  #Impostato perché non andava jquery su heroku
+  #Impostato perché non andava ckeditor su heroku
   config.assets.compile = true
 
   # Generate digests for assets URLs.
